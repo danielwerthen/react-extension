41ae20d4005cf207385a87d76b29d91fd4694795
Daniel Lichtenstein <danielwerthen@gmail.com>
2020-03-08 14:39:09 +0100

    Basic outline

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+const reactModule = require("react");
+
+export const extensionContext = reactModule.createContext({});
+
+const reactCreateElement = reactModule.createElement;
+
+const cache: { [key: string]: React.FunctionComponent } = {};
+function _extendTag(tagName: string): React.FunctionComponent {
+  const Component = reactModule.forwardRef((props: unknown, ref: unknown) => {
+    const extender = reactModule.useContext(extensionContext);
+    const newProps = extender ? extender(props) : props;
+    return reactCreateElement(tagName, { ...newProps, ref });
+  });
+  if (process.env.NODE_ENV !== "production") {
+    Component.displayName = `Extended(${tagName})`;
+  }
+  return Component;
+}
+
+export default function extendTag(tagName: string): React.FunctionComponent {
+  return cache[tagName] || (cache[tagName] = _extendTag(tagName));
+}
+
+reactModule.createElement = function extendedCreateElement(
+  tag: any,
+  props: any,
+  ...children: any
+) {
+  const newTag = typeof tag === "string" ? extendTag(tag) : tag;
+  return reactCreateElement(newTag, props, ...children);
+};
+
+export const ExtensionProvider = extensionContext.Provider;
